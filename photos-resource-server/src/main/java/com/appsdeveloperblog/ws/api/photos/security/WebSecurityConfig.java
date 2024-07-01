383544259b32a052e70780561fb1d62da65ebe1c
Isahann Hanacleto <isahanacleto@gmail.com>
2024-07-01 20:08:29 -0300

    projects

---
+++ photos-resource-server/src/main/java/com/appsdeveloperblog/ws/api/photos/security/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.appsdeveloperblog.ws.api.photos.security;
+
+import com.appsdeveloperblog.ws.api.photos.security.KeycloakRoleConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        final JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
+        http.authorizeHttpRequests((auth) ->auth.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+
+        return http.build();
+    }
+
+}
